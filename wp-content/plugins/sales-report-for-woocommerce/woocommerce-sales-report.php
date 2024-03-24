a6430a654fcee0884e002c52aa19b2cc20b1c341
DESKTOP-1N55OQJ\CY <kyi@growhub.tech>
2024-03-24 21:22:42 +0800

    add files

---
+++ wp-content/plugins/sales-report-for-woocommerce/woocommerce-sales-report.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Sales Report for WooCommerce
+ * Plugin URI: https://wordpress.org/plugins/sales-report-for-woocommerce/
+ * Description: Generates daily, weekly and monthly sales report
+ * Version: 3.5.7.8
+ * Author: BeRocket
+ * Requires at least: 5.0
+ * Author URI: https://berocket.com?utm_source=free_plugin&utm_medium=plugins&utm_campaign=sales_report
+ * Text Domain: sales-report-for-woocommerce
+ * Domain Path: /languages/
+ * WC tested up to: 8.4
+ */
+define( "BeRocket_sales_report_version", '3.5.7.8' );
+define( "BeRocket_sales_report_file", __FILE__ );
+include_once('main.php');
